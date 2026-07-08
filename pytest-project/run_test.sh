f22d076f8ad55d3f51e2076593b440f5ead596e1
Carlos Florez <ancoder29@gmail.com>
2026-07-08 16:29:29 -0500

    Agregar verificación y creación del entorno virtual en run_test.sh

--- pytest-project/run_test.sh
+++ pytest-project/run_test.sh
@@ -1,9 +1,16 @@
 #!/bin/bash
 
 echo "activando entorno virtual"
+
+if [ ! -d .venv ]; then
+    echo "Creando entorno virtual"
+    python3 -m venv .venv
+fi
+
 . .venv/bin/activate
 
 echo "Instalando dependencias"
+pip install --upgrade pip
 pip install -r requirements.txt
 
 echo "Ejecutando pruebas con pytest"
